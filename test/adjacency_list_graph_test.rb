bb75f8d12198ddde84752632aea338853a931095
selfmadekirich <tosha.kirichenko.2001@mail.ru>
2021-11-04 20:28:03 +0300

    almost all tests

--- test/adjacency_list_graph_test.rb
+++ test/adjacency_list_graph_test.rb
@@ -1,4 +1,4 @@
-require "./test/test_helper"
+require './test/test_helper'
 require 'json'
 
 class AdjacencyListGraphTest < Minitest::Test
@@ -7,15 +7,13 @@ class AdjacencyListGraphTest < Minitest::Test
   def setup
     @filepath = 'test/resources/test_data.json'
     @output_file_path = 'test/resources/output_test_data.json'
-    #if File.exists? @output_file_path
-    #  File.delete @output_file_path
-    #end
+    File.delete @output_file_path if File.exists? @output_file_path
   end
 
   def test_json_file_created_when_graph_was_dumped_to_json
     graph = Graph.new(@filepath)
     graph.dump_to_json(@output_file_path)
-    assert_equal true, File.exists?(@output_file_path), "json file was not created"
+    assert_equal true, File.exists?(@output_file_path), 'json file was not created'
   end
 
   def test_dumped_graph_in_json_file_is_correct
@@ -28,7 +26,7 @@ class AdjacencyListGraphTest < Minitest::Test
       dumped_graph_json = JSON.load(dumped_file)
       init_file.close
       dumped_file.close
-      assert_equal initial_graph_json, dumped_graph_json, "dump_to_json is not working"
+      assert_equal initial_graph_json, dumped_graph_json, 'dump_to_json is not working'
 
   end
 
@@ -46,24 +44,40 @@ class AdjacencyListGraphTest < Minitest::Test
     assert_equal 1, graph.edges.count([1,2])
   end
 
-  #def test_non_existent_edge_will_be_inserted
+  def test_non_existent_edge_will_be_inserted
+    graph = Graph.new(@filepath)
+    graph.insert_edge([1,4])
+    assert_equal true, graph.edges.include?([1, 4]), 'new edge was not added'
+  end
 
-  #end
+  def test_existing_vertice_will_not_be_inserted
+    graph = Graph.new(@filepath)
+    graph.insert_vertex(1)
+    assert_equal 1, graph.vertices.count(1), 'existing vertex has been added twice'
+  end
 
-  #def test_existing_vertice_will_not_be_inserted
+  def test_non_existing_vertice_will_be_inserted
+    graph = Graph.new(@filepath)
+    graph.insert_vertex(10)
+    assert_equal true,graph.vertices.include?(10), 'vertex was not added'
+  end
 
-  #end
+  def test_non_existent_vertice_will_be_added_while_inserting_edge
+    graph = Graph.new(@filepath)
+    graph.insert_edge([1,4])
+    assert_equal true, graph.vertices.include?(4), 'new vertex was not added during edge inserting'
+  end
 
-  #def test_existing_vertice_will_not_be_inserted
+  def test_loop_edge_will_be_added
+    graph = Graph.new(@filepath)
+    graph.insert_edge([1, 1])
+    assert_equal true, graph.edges.include?([1,1]), 'looped edge was not added'
+  end
 
-  #end
-
-  #def test_non_existent_vertice_will_be_added_while_inserting_edge
-
-  #end
-
-  #def test_existent_vertice_will_not_be_added_while_inserting_edge
-
-  #end
+  def test_loop_edge_will_not_add_same_vertex_twice
+    graph = Graph.new(@filepath)
+    graph.insert_edge([4, 4])
+    assert_equal 1, graph.vertices.count(4), 'more than 1 vertex was added during looped-edge inserting'
+  end
 
 end
