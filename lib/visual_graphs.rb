1355605d8df74c44ad9d0c1fe8f8f2536a03d3fa
selfmadekirich <tosha.kirichenko.2001@mail.ru>
2021-11-04 16:05:08 +0300

    implement graph version without constructor overrride

--- lib/visual_graphs.rb
+++ lib/visual_graphs.rb
@@ -4,4 +4,7 @@ require_relative "visual_graphs/version"
 require_relative "visual_graphs/graph"
 module VisualGraphs
   class Error < StandardError; end
+
+  class InvalidJSONFileNameError < RuntimeError; end
+
 end
--- lib/visual_graphs/graph.rb
+++ lib/visual_graphs/graph.rb
@@ -16,11 +16,50 @@ module VisualGraphs
       end
     end
 
-    def vertices()
+
+    def vertices
       @adjacency_list.keys
     end
 
-    def edges()
+    def insert_vertex(vertex)
+      unless @adjacency_list.keys.include? vertex
+        @adjacency_list[vertex] = []
+        return true
+      end
+      false
+    end
+
+    # edge is array [1,[2,3]] , where 1 and 2 are vertex , 3 - edge weight
+    # also will insert vertexes unless they are not in adjecency_list
+    def insert_edge(edge)
+      unless @edges.include? edge
+        insert_vertex(edge[0])
+        insert_vertex(edge[1])
+        @edges << edge
+        return true
+      end
+      false
+    end
+
+    def output_to_standard_stream
+      @adjacency_list.each do |vertex, adjacency_list|
+        puts "#{vertex} : #{adjacency_list}"
+      end
+    end
+
+    def correct_path?(path)
+      path.is_a? String and path.end_with? '.json'
+    end
+
+    # will create json file and dump graph to it
+    # throws No such file or directory error
+    def dump_to_json(path)
+      raise InvalidJSONFileNameError unless correct_path? path
+
+      File.open(path, 'w') { |f| f.write(@adjacency_list.to_json) }
+    end
+
+    def edges
       @edges
     end
   end
