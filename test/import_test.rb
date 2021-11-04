c60dff4ef2978df69c59317f4493b28e021840ea
selfmadekirich <tosha.kirichenko.2001@mail.ru>
2021-11-04 13:49:01 +0300

    fix problem : cannot load test_helper

--- test/import_test.rb
+++ test/import_test.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require "./test/test_helper"
 
 class ImportGraphTest < Minitest::Test
   include VisualGraphs
@@ -10,12 +10,12 @@ class ImportGraphTest < Minitest::Test
 
   def test_json_import_vertices_size
     graph = Graph.new(@filepath)
-    assert_equal 3, graph.vertices.size, "vertices size do not match"
+    assert_equal 3, graph.vertices.size, 'vertices size do not match'
   end
 
   def test_json_import_edges_size
     graph = Graph.new(@filepath)
-    assert_equal 3, graph.edges.size, "edges size do not match"
+    assert_equal 3, graph.edges.size, 'edges size do not match'
   end
 
 end
