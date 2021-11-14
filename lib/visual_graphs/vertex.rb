6f11e07ccbc740593fba84dd98f8b2abfd82616c
darkwindyclover <darkwindyclover@gmail.com>
2021-11-14 19:40:54 +0300

    Second vertex
    second implementation of vertex. Maybe I should set properties to private and add getters/setters to value and color? Otherwise I won't be able to pass tests with private access.

--- lib/visual_graphs/vertex.rb
+++ lib/visual_graphs/vertex.rb
@@ -1,10 +1,16 @@
 module VisualGraphs
   class Vertex
+    private
     attr_accessor :name
+    public
     attr_accessor :value
     attr_accessor :color
 
+    public
     def initialize(params = {})
+      check_argument_is_a_numeric(params.fetch(:value))
+      check_argument_is_a_string(params.fetch(:name, ""))
+      check_argument_is_a_string(params.fetch(:color, "black"))
       @name = params.fetch(:name, "")
       @value = params.fetch(:value)
       @color = params.fetch(:color, "black")
@@ -18,9 +24,16 @@ module VisualGraphs
       @name = name
     end
 
-    def output_to_standard_stream()
+    def print_to_output
       puts "Vertex name: #{name}, value: #{value}, color: #{color}"
     end
 
+    private
+    def check_argument_is_a_numeric(arg)
+      raise TypeError, "Invalid type of argument (Must be Numeric)" unless arg.is_a?(Numeric)
+    end
+    def check_argument_is_a_string(arg)
+      raise TypeError, "Invalid type of argument (Must be String)" unless arg.is_a?(String)
+    end
   end
 end
--- test/vertex_test_test.rb
+++ test/vertex_test_test.rb
@@ -16,4 +16,15 @@ class VertexTestTest < Minitest::Test
     assert_equal v1.value, 10, "value isn't matching"
     assert_equal v1.color, "red", "color isn't matching"
   end
+  def test_vertex_default_init
+    v2 = Vertex.new(:value => -1)
+    assert_equal v2.name, "", "name isn't matching"
+    assert_equal v2.value, -1, "value isn't matching"
+    assert_equal v2.color, "black", "color isn't matching"
+  end
+  def test_vertex_setter
+    v1 = Vertex.new(:name => "name", :value => 10, :color => "red")
+    v1.name = "newname"
+    assert_equal v1.name, "newname", "name isn't matching"
+  end
 end
