fc4eb277768ae4e60092cdfc70875d90547d7945
Geluos <rodion.bezuglov@yandex.ru>
2021-12-28 11:44:25 +0300

    improve Dijkstra tests

--- test/algorithms_test.rb
+++ test/algorithms_test.rb
@@ -5,14 +5,27 @@ require './lib/visual_graphs/weighted_graph'
 module VisualGraphs
   class AlgorithmsTest < Minitest::Test
     def setup
-      @filepath = 'test/resources/test_weighted_graph.json'
+      @filepathToWeighted = 'test/resources/test_weighted_graph.json'
+      @filepath = 'test/resources/test_data.json'
     end
 
 
-    def basic_test
-      graph = WeightedGraph.load_from_json(@filepath)
-      dist = Algorithms.dijkstra(graph, 0)
+    def basic_test_Dijkstra
+      graph = WeightedGraph.load_from_json(@filepathToWeighted)
+      dist = Algorithms.dijkstra(graph, "1")
       assert_equal [0, 3, 5], dist, 'wrong dist'
+    end
+
+    def wrong_graph_Dijkstra
+      assert_raises WrongParamsForAlgorithm do
+        graph = Graph.load_from_json(@filepath)
+        Algorithms.dijkstra(graph, "1")
+      end
+
+      assert_raises WrongParamsForAlgorithm do
+        graph = WeightedGraph.load_from_json(@filepathToWeighted)
+        Algorithms.dijkstra(graph, "120")
+      end
     end
   end
 end
