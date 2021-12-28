a6775412726406e9dcf76b6c015b63e8aabcf1fc
Geluos <rodion.bezuglov@yandex.ru>
2021-12-28 11:34:42 +0300

    fix dijkstra's alg

--- lib/visual_graphs/algorithms.rb
+++ lib/visual_graphs/algorithms.rb
@@ -4,9 +4,10 @@ module  VisualGraphs
   #Algorihtms class for Visual Graph
   class Algorithms
 
+    INF = 1000000000
+
     def init
       #infinity value
-      @inf = 1000000000
     end
 
 
@@ -17,45 +18,44 @@ module  VisualGraphs
         raise WrongParamsForAlgorithm
       end
 
-      if start < 0 or start > wh_gr.length
+      unless wh_gr.adjacency_list.has_key?(start)
         raise WrongParamsForAlgorithm
       end
 
     end
 
     def all_weight_positive(wh_gr)
-      wh_gr[v].each do |t|
-        if t.last <= 0
-          return false
+      wh_gr.edges.each{ |t|
+        t.each do
+          if t.is_a? Array and t.length == 2 and t[1] <= 0
+            return false
+          end
         end
-      end
+      }
       true
     end
 
     #Dijkstra alg
     # return distance from spec vertex for others
-    # start vertex nums from 0
     def dijkstra(wh_gr, start)
 
       check_graph_for_dijkstra(wh_gr, start)
 
+      #Convert to hash
       number_of_element = wh_gr.adjacency_list.length
-      dist = Array.new(number_of_element, @inf)
-      flags = Array.new(number_of_element, false)
+      dist = Hash.new(INF)
+      flags = Hash.new(false)
 
       dist[start] = 0
 
       number_of_element.times do
-        v = find_uncheck_min(number_of_element, flags, dist)
+        v = find_uncheck_min(wh_gr, flags, dist)
 
         flags[v] = true
 
-        #problem with no 0,1,2... graph
-        #!Need to fix logic
-        wh_gr.to_a[v].each_with_index { |g, index|
-
-          if dist[v] + g[1][1] < dist[g[1][0]]
-            dist[g[1][0]] = dist[v] + g[1][1]
+        wh_gr.adjacency_list[v].each{ |g|
+          if dist[v] + g[1] < dist[g[0]]
+            dist[g[0]] = dist[v] + g[1]
           end
         }
       end
@@ -63,9 +63,9 @@ module  VisualGraphs
 
     end
 
-    def find_uncheck_min(number_of_element, flags, dist)
-      v = -1
-      [0..number_of_element-1].each { |j|
+    def find_uncheck_min(wh_gr, flags, dist)
+      v = "-1"
+      wh_gr.vertices.each { |j|
         if not flags[j] and (v == -1 or dist[j] < dist[v])
           v = j
         end
